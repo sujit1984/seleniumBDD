73d9db620945cad960f44c9fcb7bda0c865d6114
suijt1984 <sujitsugaathan2002@gmail.com>
2024-03-25 19:36:49 +0530

    This is an initial commit to check for access files from different folders

---
+++ addition/addNums.py
@@ -0,0 +1,5 @@
+def add_two_nums(x,y):
+    return x+y
+
+def add_three_nums(x,y,z):
+    return x*y
---
+++ multiplication/multiplyNums.py
@@ -0,0 +1,5 @@
+def mult_two_nums(x,y):
+    return x*y
+
+def mult_three_nums(x,y,z):
+    return x*y*z
