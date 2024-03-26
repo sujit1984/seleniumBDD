48177b4506b1f14c0c0c568560778f1c7c7d6cd8
suijt1984 <sujitsugaathan2002@gmail.com>
2024-03-26 20:56:04 +0530

    Implemented the else block

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -5,5 +5,7 @@ try:
     print("The result is ", div)
 except ZeroDivisionError:
     print("The number cannot be divided by 0")
+else:
+    print("Inside the else block")
 
 print("The program has ended")
